0f338bbd8f20f325a33d2eef1dce68fe9139cc6c
DaniMerino <dmerino@ayesa.com>
2023-06-01 13:31:16 +0200

    bootstrap+navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailUserComponent } from './component/detail-user/detail-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'usuario/:id', component: DetailUserComponent },
+  /*
+  { path: '', component: LoginComponent },
+  { path: 'productos', component: ListaProductosComponent },
+  { path: 'producto/:id', component: ProductItemComponent },
+  { path: 'usuarios', component: ListaUsuariosComponent },
+  { path: 'usuarios/add', component: AddUserComponent },
+  { path: 'usuario/:id', component: DetalleUsuarioComponent },
+  { path: 'usuarios/edit/:id', component: EditUserComponent },
+  { path: 'usuarios/delete/:id', component: DeleteUserComponent },
+  */
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

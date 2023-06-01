1514e6af650ea20d57ae09fd89da5d4a55a7c544
andres <jamonserrate@ayesa.com>
2023-06-01 16:38:25 +0200

    Enlazando Login con register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { ListResourcesComponent } from './component/list-resources/list-resources.component';
+import { DetailResourceComponent } from './component/detail-resource/detail-resource.component';
+import { ListUsersComponent } from './component/list-users/list-users.component';
+import { DetailUserComponent } from './component/detail-user/detail-user.component';
+import { EditUserComponent } from './component/edit-user/edit-user.component';
+import { RegisterComponent } from './component/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'resources', component: ListResourcesComponent },
+  { path: 'resource/:id', component: DetailResourceComponent },
+  { path: 'users', component: ListUsersComponent },
+  { path: 'user/:id', component: DetailUserComponent },
+  { path: 'user/edit/:id', component: EditUserComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
